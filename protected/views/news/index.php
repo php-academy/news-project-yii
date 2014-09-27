f97ceca9119d72fcd94ef6f2f5d6b5a67c5c1eed
nasedkin <nasedkin.d.s@gmail.com>
2014-09-27 10:24:40 +0700

    Выносим вывод одной новости в отдельную вьюху

--- protected/views/news/index.php
+++ protected/views/news/index.php
@@ -5,15 +5,11 @@
  * Date: 27.09.14
  * Time: 8:09
  *
+ * @var NewsController $this
  * @var array $items
  * @var NewsItem $item
  */
 
-foreach( $items as $item ) { ?>
-    <p><i><?=$item->formatDate()?></i> <b><?=$item->title?></b></p>
-    <p>
-        <?=$item->shortText()?>
-    </p>
-    <p><a href="/news/item/<?=$item->newsId?>">подробнее</a></p>
-    <hr>
-<? }
+foreach( $items as $item ) {
+    $this->renderPartial('_newsItem', array('item' => $item));
+}
