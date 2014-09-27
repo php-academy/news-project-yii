50c1ad40d96ef68dfa4d0fd30e4f24c48d196817
nasedkin <nasedkin.d.s@gmail.com>
2014-09-27 09:43:35 +0700

    Настраиваем Gii и генерируем NewsItem

--- protected/models/NewsItem.php
+++ protected/models/NewsItem.php
@@ -1,27 +1,99 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: nasedkin
- * Date: 27.09.14
- * Time: 8:55
- */
 
 /**
- * Class NewsItem
+ * This is the model class for table "np_news_item".
+ *
+ * The followings are the available columns in table 'np_news_item':
  * @property integer $newsId
- * @property string  $title
- * @property string  $text
- * @property string  $publish_date
+ * @property string $title
+ * @property string $text
+ * @property string $publish_date
  */
-class NewsItem extends CActiveRecord {
-
-    public static function model($className=__CLASS__)
-    {
-        return parent::model($className);
-    }
-
+class NewsItem extends CActiveRecord
+{
+    /**
+     * @return string the associated database table name
+     */
     public function tableName()
     {
         return 'np_news_item';
+    }
+
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('title, text, publish_date', 'required'),
+            // The following rule is used by search().
+            // @todo Please remove those attributes that should not be searched.
+            array('newsId, title, text, publish_date', 'safe', 'on'=>'search'),
+        );
+    }
+
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+        );
+    }
+
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+            'newsId' => 'ID нововсти',
+            'title' => 'Название',
+            'text' => 'Содержание',
+            'publish_date' => 'Дата публикация',
+        );
+    }
+
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     *
+     * Typical usecase:
+     * - Initialize the model fields with values from filter form.
+     * - Execute this method to get CActiveDataProvider instance which will filter
+     * models according to data in model fields.
+     * - Pass data provider to CGridView, CListView or any similar widget.
+     *
+     * @return CActiveDataProvider the data provider that can return the models
+     * based on the search/filter conditions.
+     */
+    public function search()
+    {
+        // @todo Please modify the following code to remove attributes that should not be searched.
+
+        $criteria=new CDbCriteria;
+
+        $criteria->compare('newsId',$this->newsId);
+        $criteria->compare('title',$this->title,true);
+        $criteria->compare('text',$this->text,true);
+        $criteria->compare('publish_date',$this->publish_date,true);
+
+        return new CActiveDataProvider($this, array(
+            'criteria'=>$criteria,
+        ));
+    }
+
+    /**
+     * Returns the static model of the specified AR class.
+     * Please note that you should have this exact method in all your CActiveRecord descendants!
+     * @param string $className active record class name.
+     * @return NewsItem the static model class
+     */
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
     }
 }
