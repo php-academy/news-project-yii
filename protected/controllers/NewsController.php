3df99d2d19dd37687c554cfd27438b1dad5c57cd
nasedkin <nasedkin.d.s@gmail.com>
2014-09-27 08:09:06 +0700

    Добавляем NewsController

---
+++ protected/controllers/NewsController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nasedkin
+ * Date: 27.09.14
+ * Time: 8:06
+ */
+
+class NewsController extends Controller{
+
+    public function actionIndex() {
+        echo "News controller";
+    }
+} 
