82777cfa2adc57dacc7d6970eaad022348cf2816
Yuriy Vlasov <u.vlasov@gmail.com>
2014-09-30 21:06:39 +0700

    Доработка таблицы добавлен ИД коментария

--- protected/migrations/m140929_194824_create_tbl_comment_news.php
+++ protected/migrations/m140929_194824_create_tbl_comment_news.php
@@ -5,11 +5,12 @@ class m140929_194824_create_tbl_comment_news extends CDbMigration{
     public function up()
     {
         $this->createTable('np_tbl_comment_new', array(
-            'userId'      => 'int(32) not null default "user"',
+            'commentId'   => 'int(32) not null auto_increment',
+            'userId'      => 'int(32) not null',
             'newsId'      => 'int(32) not null',
             'comment'     => 'text not null',
             'publishDate' => 'datetime not null',
-            'PRIMARY KEY (newsId)'
+            'PRIMARY KEY (commentId)'
         ), 'ENGINE=InnoDB DEFAULT CHARSET=utf8');
 
         return true;
