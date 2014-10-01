e7c97d4bffad714ef5b1649fc68a883c40847971
Lev Sizov <sizov.rob2011@yandex.ru>
2014-10-01 17:06:02 +0700

    Fixed database connection for student04

--- protected/config/database.php
+++ protected/config/database.php
@@ -2,7 +2,7 @@
 
 // This is the database connection configuration.
 return array(
-    'connectionString' => 'mysql:host=localhost;dbname=student01',
+    'connectionString' => 'mysql:host=localhost;dbname=student04',
     'emulatePrepare' => true,
     'username' => 'root',
     'password' => '',
